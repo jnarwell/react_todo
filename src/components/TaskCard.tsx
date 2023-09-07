b02a0d61ff3258c948ed62af1361decdd6de9055
James Marwell <jcmarwell@gmail.com>
2023-09-07 09:09:54 -0600

    added delete and partial edit

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,3 +1,9 @@
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form'
+
+
+
 type Task = {
     id:number,
     title:string
@@ -5,14 +11,21 @@ type Task = {
 
 type TaskCardProps = {
     task:Task
+    handleDelete:(e:any)=>void
+    handleEdit:(e:any)=>void
 }
 
-export default function TaskCard({ task }: TaskCardProps){
+export default function TaskCard({ task, handleEdit, handleDelete }: TaskCardProps){
+
     return (
-        <div className="task-card text-center">
-            <div>
-                <h3>{task.title}</h3>
-            </div>
-        </div>
+        <Card key={task.id} id={task.id.toString()}>
+            <Card.Body className='d-flex justify-content-between'>
+                <Card.Title>{task.title} </Card.Title>
+                <div>
+                    <Button onClick={handleEdit} className='me-1 btn-secondary'>edit</Button>
+                    <Button onClick={handleDelete} className='btn-danger'>delete</Button>
+                </div>
+            </Card.Body>
+        </Card>
     )
 }
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import TaskCard from "../components/TaskCard";
 import TaskForm from "../components/TaskForm";
+import { findDOMNode } from "react-dom";
 
 type Task = {
     id:number,
@@ -24,11 +25,25 @@ export default function Home(){
         setNewTask({id: tasks.length + 2, title: ''})
     }
 
+    const handleDelete = (event:any) => {
+        const nodeId = Number(event.target.parentNode.parentNode.parentNode.getAttribute('id'));
+        tasks.filter((t)=>{if(t.id == nodeId){tasks.splice(tasks.indexOf(t), 1)}});
+        //tasks.map((t, id=1)=>{t.id=id;id++;})
+        setTasks(tasks)
+        setNewTask({id: tasks.length + 2, title: ''})
+    }
+
+    const handleEdit = (event: any) => {
+        const node = event.target.parentNode.previousSibling.innerText;
+        console.log(node);
+    }
+
     return (
         <>
             <h1>Hello {name}</h1>
             <TaskForm handleChange={handleInputChange} handleSubmit={handleFormSubmit} newTask={newTask}/>
-            {tasks.map(t=><TaskCard task={t} key={t.id}/>)}
+            {tasks.map((t, id=1)=>{t.id=id;id++;})}
+            {tasks.map(t=><TaskCard task={t} key={t.id} handleEdit={handleEdit} handleDelete={handleDelete}/>)}
         </>
     )
 }

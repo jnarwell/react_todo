9fcdedfa14a050348c055a63adce84ed4d81ef78
James Marwell <jcmarwell@gmail.com>
2023-09-07 14:10:29 -0600

    finished edit

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -11,19 +11,32 @@ type Task = {
 
 type TaskCardProps = {
     task:Task
-    handleDelete:(e:any)=>void
-    handleEdit:(e:any)=>void
+    handleDelete:(e:any)=>void,
+    handleEdit:(e:any)=>void,
+    handleEditSubmit:(e:any)=>void,
+    editTask:Task
 }
 
-export default function TaskCard({ task, handleEdit, handleDelete }: TaskCardProps){
+export default function TaskCard({ task, handleEdit, handleDelete, handleEditSubmit, editTask }: TaskCardProps){
 
     return (
         <Card key={task.id} id={task.id.toString()}>
-            <Card.Body className='d-flex justify-content-between'>
-                <Card.Title>{task.title} </Card.Title>
+            <Card.Body className='justify-content-between is-visible'>
+                <Card.Title id='task-title'>{task.title}</Card.Title>
                 <div>
                     <Button onClick={handleEdit} className='me-1 btn-secondary'>edit</Button>
                     <Button onClick={handleDelete} className='btn-danger'>delete</Button>
+                </div>
+            </Card.Body>
+            <Card.Body className='justify-content-between is-invisible'>
+                <Card.Title>
+                    <Form>
+                        <Form.Control name='title' onChange={handleEdit} value={editTask.title} className='w-100'/>
+                    </Form>
+                </Card.Title>
+                <div>
+                    <Button onClick={handleEditSubmit} className='me-1 btn-secondary'>submit</Button>
+                    <Button className='btn-danger'>cancel</Button>
                 </div>
             </Card.Body>
         </Card>
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import TaskCard from "../components/TaskCard";
 import TaskForm from "../components/TaskForm";
-import { findDOMNode } from "react-dom";
 
 type Task = {
     id:number,
@@ -12,7 +11,8 @@ export default function Home(){
     const name:string = 'Jamie';
     const [tasks, setTasks] = useState<Task[]>([]);
     const [newTask, setNewTask] = useState<Task>({id:1, title:''});
-    
+    const [editTask, setEditTask] = useState<Task>({id:1, title:''});
+
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         console.log(event.target.value, event.target.name);
         setNewTask({...newTask, [event.target.name]: event.target.value})
@@ -34,8 +34,39 @@ export default function Home(){
     }
 
     const handleEdit = (event: any) => {
-        const node = event.target.parentNode.previousSibling.innerText;
-        console.log(node);
+        
+        if (editTask.title == ''){
+            const regBar = event.target.parentNode.parentNode;
+            const editBar = event.target.parentNode.parentNode.nextSibling;
+    
+            regBar.classList.add('is-invisible');
+            regBar.classList.remove('is-visible');
+            editBar.classList.add('is-visible');
+            editBar.classList.remove('is-invisible');
+            const node = event.target.parentNode.previousSibling.innerHTML;
+            const nodeId = Number(event.target.parentNode.parentNode.parentNode.getAttribute('id'));
+
+            let initTask:Task = {id:nodeId, title:node};
+            setEditTask(initTask)
+        }
+        else {setEditTask({...editTask, [event.target.name]: event.target.value})}
+    }
+    
+    const handleEditSubmit = (event:any) => {
+        const editBar = event.target.parentNode.parentNode;
+        const regBar = event.target.parentNode.parentNode.previousSibling;
+    
+        regBar.classList.remove('is-invisible');
+        regBar.classList.add('is-visible');
+        editBar.classList.remove('is-visible');
+        editBar.classList.add('is-invisible');
+
+        const nodeId = Number(event.target.parentNode.parentNode.parentNode.getAttribute('id'));
+        console.log(nodeId)
+        tasks.filter((t)=>{if(t.id == nodeId){t.title = editTask.title}});
+        editTask.title = ''
+        setTasks(tasks)
+        setNewTask({id: tasks.length + 2, title: ''})
     }
 
     return (
@@ -43,7 +74,7 @@ export default function Home(){
             <h1>Hello {name}</h1>
             <TaskForm handleChange={handleInputChange} handleSubmit={handleFormSubmit} newTask={newTask}/>
             {tasks.map((t, id=1)=>{t.id=id;id++;})}
-            {tasks.map(t=><TaskCard task={t} key={t.id} handleEdit={handleEdit} handleDelete={handleDelete}/>)}
+            {tasks.map(t=><TaskCard task={t} key={t.id} handleEdit={handleEdit} handleDelete={handleDelete} editTask={editTask} handleEditSubmit={handleEditSubmit}/>)}
         </>
     )
 }
